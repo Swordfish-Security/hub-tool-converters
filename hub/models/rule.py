48d1886ca11d71897e7afcc707f5274e4a9040e4
Sergei Polshkov <polshkov1@mail.ru>
2024-04-09 15:08:11 +0300

    Update rule CWE parsing

--- hub/models/rule.py
+++ hub/models/rule.py
@@ -3,13 +3,31 @@ from dataclasses import dataclass
 from config.enums import ScannerTypes
 
 
+@dataclass(kw_only=False, eq=False, order=False)
+class RuleCwe:
+    id: int = 0
+    name: str | None = None
+    link: str | None = None
+
+    def __init__(
+            self,
+            id: int = 0,
+            name: str | None = None,
+            link: str | None = None
+    ):
+        self.id = id
+        self.name = name
+        self.link = link
+        super().__init__()
+
+
 @dataclass(kw_only=True, eq=False, order=False)
 class Rule:
     type: ScannerTypes | None = None
     name: str | None = None
     id: str | None = None
     severity: str | None = None
-    cwe: list[dict[str, str | int]] | None = None
+    cwe: list[RuleCwe] | None = None
     description: str | None = None
 
     def __init__(
@@ -18,7 +36,7 @@ class Rule:
             name: str | None = None,
             severity: str | None = None,
             description: str | None = None,
-            cwe: list[dict[str, str | int]] | None = None
+            cwe: int = 0
     ):
         self.type = type
         self.name = name
